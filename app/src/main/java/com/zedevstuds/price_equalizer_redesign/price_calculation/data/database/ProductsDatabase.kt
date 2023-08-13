f6411c4bfcde3df566aac8862055dadf8281aab2
zedevstuds <st2s3@mail.ru>
2023-08-13 17:16:09 +0200

    Added ExportSchema for database

--- app/src/main/java/com/zedevstuds/price_equalizer_redesign/price_calculation/data/database/ProductsDatabase.kt
+++ app/src/main/java/com/zedevstuds/price_equalizer_redesign/price_calculation/data/database/ProductsDatabase.kt
@@ -3,7 +3,7 @@ package com.zedevstuds.price_equalizer_redesign.price_calculation.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [ProductDbModel::class, ListDbModel::class], version = 1, exportSchema = false)
+@Database(entities = [ProductDbModel::class, ListDbModel::class], version = 1, exportSchema = true)
 abstract class ProductsDatabase : RoomDatabase() {
     abstract val productsDao: ProductsDao
     abstract val listsDao: ListsDao
